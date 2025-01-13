18e3d59c44e4f33a9ea0f6c9f6119187fba37707
Barbara Michel <barbmichel22@gmail.com>
2025-01-13 14:27:30 +0000

    Secure secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--sq1%yd$4*(+vzev*lipndgv5g^16avxm8=-bb5ygpg)kxg%g5'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -89,7 +89,6 @@ DATABASES = {
     'default':
     dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
