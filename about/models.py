834bee907fd0bf918c8315add954e1edb3dfbd50
Barbara Michel <barbmichel22@gmail.com>
2025-01-26 17:40:19 +0000

    Add about information to test display of about secion

--- about/models.py
+++ about/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class About(models.Model):
     title = models.CharField(max_length=200, unique=True)
     content=models.TextField()
-    created_on=models.DateTimeField(auto_now_add=True)
+    updated_on=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
