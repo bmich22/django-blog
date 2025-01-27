2cc97c8bcf71d58e5913b4e6023688afac6045d1
Barbara Michel <barbmichel22@gmail.com>
2025-01-27 12:45:30 +0000

    Style sign up page

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-bmich22-djangoblog-gg9eub0k1l2.ws.codeinstitute-ide.net",".herokuapp.com"]
 
