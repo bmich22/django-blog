2017c3b0b7ed93a3e1b3cdbadd86b71c8084b204
Barbara Michel <barbmichel22@gmail.com>
2025-01-28 18:57:51 +0000

    Add collaboration request form to about section

--- blog/views.py
+++ blog/views.py
@@ -29,6 +29,7 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
+        print('Recieved a post request')
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -38,7 +39,8 @@ def post_detail(request, slug):
             messages.add_message(request, messages.SUCCESS,'Comment submitted and awaiting approval')
 
     comment_form = CommentForm()
-
+    print("About to render template")
+    
     return render(
         request,
         "blog/post_detail.html",
