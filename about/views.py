6e0b0aa0e08f2b925dabbbdcdff93927dcfb77d8
Barbara Michel <barbmichel22@gmail.com>
2025-01-30 12:14:45 +0000

    Add testing

--- about/views.py
+++ about/views.py
@@ -24,7 +24,7 @@ def about_me(request):
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
             collaborate_form.save()
-            messages.add_message(request, messages.SUCCESS,"Collaboration request received! I endeavor to respond within 2 working days.")
+            messages.add_message(request, messages.SUCCESS,"Collaboration request received! I endeavour to respond within 2 working days.")
 
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
